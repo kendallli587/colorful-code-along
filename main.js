88786e65c6637ae66dabe3efd29f41487f4182dd
kendallli587 <kendallli587@gmail.com>
2022-07-06 10:24:19 -0600

    .includes() and .split()

--- main.js
+++ main.js
@@ -7,25 +7,44 @@ let inputEl = document.getElementById("color-in");
 // Global Variable
 let colors = [];
 
+// Fetch Content from .txt file
+fetch("colours.txt").then(convertData).then(processData);
+
+function convertData(rawData) {
+    return rawData.text();
+}
+
+function processData(stringData) {
+    // \r\n is the read line character and new line character
+    // Question mark makes looking for \r character optional, 'n always there
+    colors = stringData.split(/\r?\n/); // Split lines into array
+    displayColors(colors);
+}
+
 // Event Listener
-inputEl.addEventListener("keydown", submitHandler);
+inputEl.addEventListener("keyup", submitHandler);
 
 function submitHandler(event) {
-    if (event.keyCode === 13) {
-        // Add user's input to colors array and display
-        colors.push(inputEl.value);
-        inputEl.value = "";
-        displayColors(colors);
+    // Display all colors on the page
+    let divStr = "";
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i].includes(inputEl.value)) {
+            divStr += `<div style="background: ${colors[i]}"></div>`;
+        }
     }
+    containerEl.innerHTML = divStr;
+
 }
 
 function displayColors(colors) {
     // Display all colors on the page
     let divStr = "";
+    let count = 0;
     for (let i = 0; i < colors.length; i++) {
         divStr += `<div style="background: ${colors[i]}"></div>`;
+        count++
     }
-    containerEl.innerHTML = divStr;
+    containerEl.innerHTML = divStr + `<p>Count: ${count}</p>`;
 }
 
 
