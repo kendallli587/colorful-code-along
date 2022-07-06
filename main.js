ad4df8f4fe41cb8d107685690cf19de55e647a89
kendallli587 <kendallli587@gmail.com>
2022-07-06 09:41:45 -0600

    update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+// Color Arrays
+
+// Variables for HTML elements
+let containerEl = document.getElementById("container");
+
+// Array
+let colors = ["red", "green", "blue", "orange", "cyan", "purple", "maroon"];
+// Index ->     0        1       2        3        4       5
+
+
+// Display all colors on the page
+let divStr = "";
+for (let i = 0; i < colors.length; i++) {
+    divStr += `<div style="background: ${colors[i]}"></div>`;
+}
+containerEl.innerHTML = divStr;
+
+
+
+
+
+// Function Library
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function randomElement(anArray) {
+    return anArray[randomInt(0, anArray.length)];
+}
